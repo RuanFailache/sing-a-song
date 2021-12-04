785c64c3aa4f8abbacedd72c91bf7bebff9be11d
RuanFailache <ruanfailache@outlook.com>
2021-12-04 20:30:51 -0300

    update - all test for all errors from services

--- tests/units/recommendation.test.js
+++ tests/units/recommendation.test.js
@@ -5,8 +5,10 @@ import * as recommendationRepository from '../../src/repositories/recommendation
 
 import InvalidSongError from '../../src/errors/InvalidSongError.js';
 import AddConflictError from '../../src/errors/AddConflictError.js';
+import NotSongsFoundError from '../../src/errors/NotSongsFoundError.js';
+import InvalidAmountError from '../../src/errors/InvalidAmountError.js';
 
-describe('POST /recommendaton', () => {
+describe('POST /recommendations', () => {
   beforeAll(async () => {
     await recommendationRepository.addRecommendation(
       'Clock Strikes',
@@ -25,5 +27,53 @@ describe('POST /recommendaton', () => {
       'https://www.youtube.com/watch?v=6YZlFdTIdzM',
     );
     await expect(result).rejects.toThrowError(AddConflictError);
+  });
+});
+
+describe('POST /recommendations/:id/upvote', () => {
+  test('Invalid song id', async () => {
+    const result = recommendationService.upVote(0);
+    await expect(result).rejects.toThrowError(NotSongsFoundError);
+  });
+});
+
+describe('POST /recommendations/:id/downvote', () => {
+  test('Invalid song id', async () => {
+    const result = recommendationService.downVote(0);
+    await expect(result).rejects.toThrowError(NotSongsFoundError);
+  });
+});
+
+describe('POST /recommendations/random', () => {
+  test('Not songs added', async () => {
+    jest
+      .spyOn(recommendationRepository, 'listSongs')
+      .mockImplementation(() => []);
+
+    const result = recommendationService.random();
+    await expect(result).rejects.toThrowError(NotSongsFoundError);
+  });
+
+  test('Get a song', async () => {
+    const mockData = [{
+      name: 'Clock Strikes',
+      youtubeLink: 'https://www.youtube.com/watch?v=6YZlFdTIdzM',
+      score: 252,
+    }];
+
+    jest
+      .spyOn(recommendationRepository, 'listSongs')
+      .mockImplementation(() => mockData);
+
+    const result = await recommendationService.random();
+
+    expect(result).toBe(mockData[0]);
+  });
+});
+
+describe('POST /recommendations/top/:id', () => {
+  test('Invalid amount of songs', async () => {
+    const result = recommendationService.topSongs(0);
+    await expect(result).rejects.toThrowError(InvalidAmountError);
   });
 });
